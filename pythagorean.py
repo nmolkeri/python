cdd8843e90d6bfd18ff751e43c0d97c6f3e4c367
nmolkeri <nmolkeri@gmail.com>
2016-08-19 20:01:58 -0700

    validating pythogorean

---
+++ pythagorean.py
@@ -0,0 +1,23 @@
+print("1. ")
+choice = input("Enter a choice")
+
+num1 = input("Enter first number: ")
+num2 = input("Enter second number: ")
+num3 = input("Enter the third number: ")
+
+
+num1s = num1 * num1
+num2s = num2 * num2
+num3s = num3 * num3
+
+#addition = num1s + num2s
+
+if num1s + num2s == num3s:
+    print "This is pythogorean triangle"
+else:
+    print "Not a pythogorean triangle"
+
+
+#print "square of first number is %d "  %num1s
+#print "square of second number is %d"  %num2s
+
